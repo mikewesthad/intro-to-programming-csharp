e8b86cc7dec257b7862467de9ef08347e0ef312e
Michael Hadley <michaelwesthadley@gmail.com>
2020-01-16 12:28:52 -0600

    Add week 1 demos

---
+++ Demos/Week01/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * HelloWorld by Michael Hadley
+ * Intro to Programming, 09/05/19
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Mike's first app";
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            // Ask the console to print a greeting.
+            Console.WriteLine("Hello!");
+
+            // Wait until a key is pressed.
+            Console.ReadKey();
+
+            /*
+             * This
+             * Is
+             * A 
+             * Multi-line
+             * Comment.
+             */
+
+            // Play an annoying 10000hz sound for 2000ms (after the user has pressed a key!)
+            Console.Beep(10000, 2000);
+        }
+    }
+}
---
+++ Demos/Week01/Variables/Variables/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Integer values are whole numbers.
+            int numJupiterMoons = 79;
+            // You can concatenate a string with a number.
+            Console.WriteLine("The number of moon's Jupiter has: " + numJupiterMoons);
+            // You can also do math on numbers.
+            Console.WriteLine(numJupiterMoons + 10);
+
+            // Don't do this - type mismatch, creating an "int" variable but a "double" value.
+            //int badInt = 55.5;
+
+            // Floating point values are numbers that can have a decimal portion.
+            double salesTaxAmount = 6.25;
+            Console.WriteLine("The sales tax in Chicago: " + salesTaxAmount);
+            float cost = 10.25f;
+
+            // String values are a series of characters.
+            string quoteOfTheDay = "Perfect is the enemy of good.";
+            Console.WriteLine(quoteOfTheDay);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
