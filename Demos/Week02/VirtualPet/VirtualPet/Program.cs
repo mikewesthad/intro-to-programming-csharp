f49eea7b2523f47e04eb977475c4c6741eb5e921
Michael Hadley <michaelwesthadley@gmail.com>
2020-01-16 12:34:13 -0600

    Add week 2 demos

---
+++ Demos/Week02/VirtualPet/VirtualPet/Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Virtual Pet");
+
+            // We can create a pet and set its public fields manually.
+            VirtualPet junior = new VirtualPet();
+            junior.Name = "Junior";
+            junior.AgeInDays = 7;
+            junior.Greet();
+
+            // The power of classes is that we can now create many unique virtual pets from the 
+            // same template.
+            VirtualPet steph = new VirtualPet();
+            steph.Name = "Steph";
+            steph.AgeInDays = 73;
+            steph.Greet();
+
+            // We can use our fancy constructor to streamline the process of creating a pet -
+            // no need for accessing the Name/Mood/AgeInDays fields directly.
+            VirtualPet ruffles = new VirtualPet("Ruffles", "Chipper", 30);
+            ruffles.Greet();
+
+            // Now we can create a virtual pet for the player:
+            VirtualPet playersPet = new VirtualPet();
+            WriteLine("Please name your pet:");
+            playersPet.Name = ReadLine();
+            playersPet.Greet();
+
+            ReadKey();
+        }
+    }
+}
---
+++ Demos/Week02/VirtualPet/VirtualPet/VirtualPet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+        // Fields - variables declared within a class.
+        // Naming convention: PascalCase.
+        // [access modifier] [type] [identifier] [initializer]
+        public string Name;
+        public string Mood;
+        public int AgeInDays;
+        public bool IsAwake = false;
+        private double EnergyLevel = 1.0;
+
+        // A constructor that takes no parameters and sets some of the fields to default values.
+        public VirtualPet()
+        {
+            Name = "Unknown";
+            Mood = "Unknown";
+            AgeInDays = 0;
+        }
+
+        // A constructor that takes three parameters and uses them to set the fields.
+        public VirtualPet(string name, string mood, int ageInDays)
+        {
+            Name = name;
+            Mood = mood;
+            AgeInDays = ageInDays;
+        }
+
+        public void Greet()
+        {
+            WriteLine("Why hello, my name is " + Name + ".");
+            WriteLine("I am " + AgeInDays + " days old.");
+        }
+    }
+}
